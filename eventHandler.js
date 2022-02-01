218b0af25d5806d58babfd666603543d1e0679e8
chris <oaoxd0314@gmail.com>
2022-02-01 20:51:52 +0800

    add welcome message and join message and .env

---
+++ eventHandler.js
@@ -0,0 +1,34 @@
+const linebot = require('linebot');
+require('dotenv').config();
+
+const bot = linebot({
+    channelId: process.env.CHANNEL_ID,
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+  
+const linebotParser = bot.parser();
+
+function eventHandler() {
+    bot.on('join',function(event){
+        console.log(event)
+        event.reply('Hi 歡迎使用，如果你想玩的話到 https://github.com/oaoxd0314/line-bot-replyer 自己 clone 一份')
+    })
+    
+    bot.on('memberJoined',function(event){
+        console.log(event)
+        event.reply('近來先看記事本喔 (ゝ∀･)b 有事要跟大家商量')
+    })
+
+    bot.on('message', function(event) {
+        if (event.message.type == 'text') {
+            let msg = event.message.text;
+            if (msg === 'help'){
+                // event.reply('您可以使用:' + '</br>' +'')
+                event.reply('目前尚未有任何功能 你要幫我嗎 :<')
+            }
+        }
+      });
+}
+
+module.exports = {eventHandler,linebotParser}
--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const linebot = require('linebot');
 const express = require('express');
+const {eventHandler,linebotParser} = require('./eventHandler')
 
-const bot = linebot({
-  channelId: '1656852921',
-  channelSecret: 'd7b6ae01154c80a3200aafc694d63609',
-  channelAccessToken: 'MFDXbM2VcVcxctpAO2eIkUJzbkn+aUX4VfxdzTYml+g+nPj2794th7iiTGgZWntHfOeDwcWp0XtSIN9ReVHFZeXAzHWxvw3L6ot4FeVbVxeKPl4Fc1wGTUDGNgA+X5XcNbZ+kUbj/dmwFf9Jq/tKtAdB04t89/1O/w1cDnyilFU='
-});
-
-
-bot.on('message', function(event) {
-    console.log(event); //把收到訊息的 event 印出來看看
-});
-
-bot.on('join',function(event){
-    console.log(event)
-    event.reply('近來先看記事本喔 (ゝ∀･)b 有事要跟大家商量')
-})
+eventHandler()
   
 const app = express();
-const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
