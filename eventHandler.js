bedeaa14a403f8b58bbd59200ff05a1774e0c76a
chris <oaoxd0314@gmail.com>
2022-02-01 21:08:29 +0800

    bot test -1

--- eventHandler.js
+++ eventHandler.js
@@ -1,12 +1,19 @@
 const linebot = require('linebot');
 require('dotenv').config();
 
+console.log('event Handler')
 console.log(process.env.CHANNEL_ID)
 
+// const bot = linebot({
+//     channelId: process.env.CHANNEL_ID,
+//     channelSecret: process.env.CHANNEL_SECRET,
+//     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+// });
+
 const bot = linebot({
-    channelId: process.env.CHANNEL_ID,
-    channelSecret: process.env.CHANNEL_SECRET,
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+    channelId: '1656852921',
+    channelSecret: 'd7b6ae01154c80a3200aafc694d63609',
+    channelAccessToken: 'MFDXbM2VcVcxctpAO2eIkUJzbkn+aUX4VfxdzTYml+g+nPj2794th7iiTGgZWntHfOeDwcWp0XtSIN9ReVHFZeXAzHWxvw3L6ot4FeVbVxeKPl4Fc1wGTUDGNgA+X5XcNbZ+kUbj/dmwFf9Jq/tKtAdB04t89/1O/w1cDnyilFU='
 });
   
 const linebotParser = bot.parser();
@@ -25,6 +32,8 @@ function eventHandler() {
     bot.on('message', function(event) {
         let msg = event.message.text;
         console.log(msg)
+
+        event.reply(msg)
         if (event.message.type == 'text') {
             
             if (msg === 'help'){
