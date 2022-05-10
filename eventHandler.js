3002689e7ce9bcf08a29e3d96852d722bd9ec7d4
chris <oaoxd0314@gmail.com>
2022-05-10 14:13:32 +0800

    server test

--- eventHandler.js
+++ eventHandler.js
@@ -44,7 +44,7 @@ function eventHandler() {
 function do_post(to,msg){
     bot.push(to,{
         "type": "text",
-        "text": "$ LINE emoji $",
+        "text": `$ LINE ${msg} $`,
         "emojis": [
           {
             "index": 0,
